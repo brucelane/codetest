792e864f6a505f6986677a6d8c6e85cca7934c3a
jarpi <josep.arpi@gmail.com>
2015-09-13 17:03:49 +0200

    - Added citizen births test cases - 1 passing - 10 red

--- tests/citizenBirth.js
+++ tests/citizenBirth.js
@@ -12,7 +12,16 @@ mongoose.connect(config.db_connection);
 // Test cases ******* 
 //******************* 
 describe('Citizen births', function() {
-	describe('Add citizen birth' , function() {
+	describe('Add citizen birth when no setup loaded' , function() {
+		it('Should return an empty array of citizen births when retrieving all citizen births', function(done) {
+			done(new Error("Not implemented")); 
+		}); 
+		it('Should return an empty citizen when searching by citizen key', function(done) {
+			done(new Error("Not implemented")); 
+		}); 
+		it('Should return an error when deleting citizen by key', function(done) {
+			done(new Error("Not implemented")); 
+		}); 
 		it('Should not add a citizen birth because there aren\'t free id\'s', function(done) {
 			var citizenDataModel = new citizenModel({
 				name : "test" 
@@ -27,5 +36,32 @@ describe('Citizen births', function() {
 				done(); 
 			}); 
 		}); 
-	}); 
+	});
+	describe('Add citizen birth when setup loaded' , function() {
+		it('Should return an array of citizen births when retrieving all citizen births', function(done) {
+			done(new Error("Not implemented")); 
+		}); 
+		it('Should return a citizen when searching by citizen key', function(done) {
+			done(new Error("Not implemented")); 
+		}); 
+		it('Should return an when deleting citizen by key', function(done) {
+			done(new Error("Not implemented")); 
+		}); 
+		it('Should add citizen birth when free id available and citizen data is format compliant', function(done) {
+			done(new Error("")); 
+		}); 
+	});
+	describe('Add citizen birth when setup is loaded and data is not format compliant', function() {
+		it('Should return an Error() when searching by citizen key number', function(done) {
+			done(new Error("Not implemented")); 
+		}); 
+		it('Should return an Error() when deleting citizen by key number', function(done) {
+			done(new Error("Not implemented")); 
+		}); 
+		it('Should not add citizen birth when free id available and citizen data is not format compliant', function(done) {
+			// Try date as other format 
+			done(new Error("")); 
+		}); 
+
+	});   
 }); 
