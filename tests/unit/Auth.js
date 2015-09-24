1e9ca5abead29d1043edefa7e23a18f1533c21f8
jarpi <josep.arpi@gmail.com>
2015-09-24 11:48:13 +0200

    - Refactoring tests - Adding stubs

--- tests/unit/Auth.js
+++ tests/unit/Auth.js
@@ -2,23 +2,35 @@
 // Dependencies **** 
 //****************** 
 var should = require('should'); 
-var mongoose = require('mongoose');
-var citizenCtrl = require('../../controllers/Citizen.js'); 
-var citizenModel = mongoose.model('Citizen'); 
-var freeIdCtrl = require('../../controllers/FreeId.js');  
-var authCtrl = require('../../controllers/Auth.js'); 
+// var mongoose = require('mongoose');
+// var citizenCtrl = require('../../controllers/Citizen.js'); 
+// var citizenModel = mongoose.model('Citizen'); 
+// var freeIdCtrl = require('../../controllers/FreeId.js');  
+// var authCtrl = require('../../controllers/Auth.js'); 
 var config = require('../../config/config.js'); 
  
+var citizenCtrl = {}; 
+var citizenModel = {}; 
+var freeIdCtrl = function()  {
+	return {
+		generateNewId: function (cb) {
+			return cb(null, 1);  
+		}
+	}
+}(); 
+var authCtrl = {}; 
+
 //******************* 
 // Test cases ******* 
 //******************* 
 describe('FreeId', function() {
 	before(function(done) {
-		mongoose.connect(config.db_connection); 
+		// mongoose.connect(config.db_connection);
+		console.dir(freeIdCtrl.generateNewId);  
 		done(); 
 	}); 	
 	after(function(done) {
-		mongoose.disconnect(); 
+		// mongoose.disconnect(); 
 		done();
 	});
 	describe('Generate a valid token when a user exists' , function() {
